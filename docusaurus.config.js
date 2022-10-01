f8ec3c1153b99b563d264e23e8b87e169a61a9ec
cdshim1980 <chuldongshim@gmail.com>
2022-10-01 23:59:19 +0900

    color모드버튼 삭제

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,6 +48,11 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
           autoCollapseCategories: true,   // 펼쳐져 있는 사이드바 항목 접어서 축소
         },
       },
+      colorMode: {
+        defaultMode: 'dark',	// light or dark
+        disableSwitch: true,	// 메뉴바에서 모드 전환버튼 숨김
+        respectPrefersColorScheme: true,	// defaultMode 대신 사용자 시스템 환경 설정에 따라 color 속성 적용 안함
+      },
       navbar: {
         hideOnScroll: true,               // 아래로 스크롤 시 타이틀메뉴 숨기고, 위로 스크롤 시 타이틀메뉴 보이는 기능
         title: '나는 개발자다!',

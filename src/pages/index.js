1bf660781a092a9181a683dae25d5f02b2fed0d1
cdshim1980 <chuldongshim@gmail.com>
2023-01-21 12:46:09 +0900

    첫페이지 문구 위치 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,6 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <center>간절함과 절박함속에서도 포기하지 말고, 비교하지 말고, 나의 걸을걸이로 나의 길을 찾아서...</center>
           <Link className="button button--secondary button--lg" to="/docs/mycareer">
             1min Career ⏱️
           </Link>
@@ -39,6 +38,7 @@ export default function Home() {
               <center>개발자로서 살아온 흔적을 정리하여 나를 돌아보고,</center>
               <center>앞으로 내가 세상에 기여할 수 있는 것들이 무엇이 있을지 찾기 위한 공간입니다.</center>
             <center><font size="4" color="#34a28a"><strong>"Yesterday is History. Tomorrow is a Mystery. Today is a Gift."</strong></font></center>
+            <center>간절함과 절박함속에서도 포기하지 말고, 비교하지 말고, 나의 걸을걸이로 나의 길을 찾아서...</center>
           </>
         }
         <HomepageFeatures />

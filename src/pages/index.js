ed4609b244e605940c8d2d782a5b715444c72a1c
cdshim1980 <chuldongshim@gmail.com>
2023-01-21 09:31:40 +0900

    첫페이지 문구 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,9 @@ function HomepageHeader() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
+          <center>간절함과 절박함속에서도 포기하지 말고, 비교하지 말고, 나의 걸을걸이로 나의 길을 찾아서...</center>
           <Link className="button button--secondary button--lg" to="/docs/mycareer">
-            My Career Summary - 1min ⏱️
+            1min Career ⏱️
           </Link>
         </div>
       </div>
@@ -34,12 +35,10 @@ export default function Home() {
         {
           <>
             <br />
-            <center><font size="5" color="#34a28a"><strong>이 공간은</strong></font></center>
+              <center><font size="5" color="#34a28a"><strong>이 공간은</strong></font></center>
               <center>개발자로서 살아온 흔적을 정리하여 나를 돌아보고,</center>
               <center>앞으로 내가 세상에 기여할 수 있는 것들이 무엇이 있을지 찾기 위한 공간입니다.</center>
             <center><font size="4" color="#34a28a"><strong>"Yesterday is History. Tomorrow is a Mystery. Today is a Gift."</strong></font></center>
-            <center><font size="4" color="#34a28a"><strong>"Only one last chance in my life. If I can't do it, developer Sim Cheol-dong will no longer exist."</strong></font></center>
-            <center><font size="4" color="#34a28a"><strong>"With more urgency and earnestness than anyone else"</strong></font></center>
           </>
         }
         <HomepageFeatures />
